25bd8022244d3897fc105f70347951070b8762a2
Bae SangHyuk <44903787+D3vle0@users.noreply.github.com>
2019-06-29 00:06:08 +0900

    Add files via upload

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -8,6 +8,20 @@ var countDownDate = new Date("July 3, 2019 09:25:00").getTime();
             var minutes = Math.floor((timeleft % (1000 * 60 * 60))/ (1000 * 60));
             var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
+            hours = (hours < 10) ? "0" +  hours : hours;
+            minutes = (minutes < 10) ? "0" + minutes : minutes;
+            seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+            var ang = new Date();
+            var currentMonth = ang.getMonth();
+            var currentDate = ang.getDate();
+            var currentHour = ang.getHours();
+            if (currentMonth == 6 && currentDate >= 5 && currentHour >= 12){
+                window.location="fin.html";
+            }
+            if (currentMonth >= 7){
+                window.location="fin.html";
+            }
             document.getElementById("counter").innerHTML = '<div class="box time">' + days + '</div><div class="box time">' + hours + '</div><div class="box time">' + minutes + '</div><div class="box time">' + seconds + '</div>';
         
         }, 1000 );
